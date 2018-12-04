e9d13ac5e10d5b42ad7cbc9990810d0834006471
Igor Zahvatov <iz@nowmedia.ru>
2018-12-05 01:08:48 +0300

    Удален вывод логина и пароля к bitcoin rpc

--- views/site/index.php
+++ views/site/index.php
@@ -33,22 +33,6 @@ $this->title = 'Bitcoin client';
 			</div>
 		</div>
 		<div class="form-group">
-			<label class="col-md-2">
-				Username
-			</label>
-			<div class="col-md-4">
-				<?=$credentials->getUserName()?>
-			</div>
-		</div>
-		<div class="form-group">
-			<label class="col-md-2">
-				Password
-			</label>
-			<div class="col-md-4">
-				<?=$credentials->getPassword()?>
-			</div>
-		</div>
-		<div class="form-group">
 			<div class="col-md-4">
 				<form action="<?=Url::to(['site/index'])?>">
 					<button type="submit" class="btn btn-default">Check balance</button>
